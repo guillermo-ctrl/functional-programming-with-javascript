ea9769192efed55b19a17f24308a015618ae7d9d
Your Name <you@example.com>
2021-02-15 15:03:32 +0100

    Fixed starter code (no error messages)

--- project/src/server/index.js
+++ project/src/server/index.js
@@ -1,3 +1,5 @@
+
+//required stuff to run server
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -10,19 +12,24 @@ const port = 3000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+//This determins what happens in root. It gives access to elements in "public" folder
 app.use('/', express.static(path.join(__dirname, '../public')))
 
-// your API calls
-
-// example API call
+// example API call (apod)
 app.get('/apod', async (req, res) => {
+    // Prepare output in JSON format
+    
     try {
         let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
-        res.send({ image })
+    
+            /*console.log(image.date)
+        res.send(image.date)*/
+        res.send({image})
     } catch (err) {
         console.log('error:', err);
     }
 })
 
+// the following is the listener to start the server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
