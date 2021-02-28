fc3b3f2e12f55b429bb0728b47d451f85773abed
Your Name <you@example.com>
2021-02-28 10:57:40 +0100

    Refactor

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -7,8 +7,9 @@ const root = document.getElementById('root')
 
 // roverForm is the ID os the dropdown menu to choose a rover
 const roverForm = () => {
-    if (document.getElementById("roverDropDown")) return document.getElementById("roverDropDown")
-    else return "root"
+    //checks first if the dropdown menu is there
+    if (document.getElementById('roverDropDown')) return document.getElementById('roverDropDown')
+    else return 'root'
 }
 
 // updateStore is the only function that changes the values of the store object. It then renders the content
@@ -24,7 +25,9 @@ const render = async (root, state) => {
 
 // create content
 const App = (state) => {
-    let { rovers } = state
+    let {
+        rovers
+    } = state
 
     return `
         <header></header>
@@ -41,7 +44,7 @@ const App = (state) => {
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
-    render(root, store) 
+    render(root, store)
 })
 
 //listening for form changes to grab the chosen rovers api
@@ -49,26 +52,37 @@ root.addEventListener('change', () => {
     getRoverApi(store, roverForm().value)
 })
 
-// If there is a chosen rover in store, show the rover information, else show "choose a rover" message
+// If there is a chosen rover in store, show the rover information, else show 'choose a rover' message
 const roverInfo = (rovers) => {
     if (Array.isArray(store.rovers)) {
         return `
             <p>Choose a rover!</p>
         `
-    }
-
-    else {
+    } else {
         const latestImages = () => {
             let arr = []
+            let cams = []
             rovers.image.photos.forEach((element, index) => {
+                cams.push(rovers.image.photos[index].camera.full_name)
                 arr.push(`
-                <img src="${rovers.image.photos[index].img_src}" max-width:100%; height:auto />
-                <figcaption>Taken with the ${rovers.image.photos[index].camera.full_name}</figcaption>
+                    <img src='${rovers.image.photos[index].img_src}' max-width:100%; height:auto />
+                    <figcaption>Taken with the ${rovers.image.photos[index].camera.full_name}</figcaption>
                 `)
             });
-            return (arr)
+            const filtered = rovers.image.photos.filter((x, y, z) => {
+                if (y > 0) return x.camera.full_name != z[y - 1].camera.full_name
+                else return x = x
+            });
+
+            const map1 = filtered.map((x, y) => {
+                return (`
+                    <img src='${x.img_src}' max-width:100%; height:auto />
+                    <figcaption>Taken with the ${x.camera.full_name}</figcaption>
+                `)
+            });
+
+            return (map1)
         }
-    
         return (`
         <h1>${rovers.image.photos[0].rover.name} rover</h1>
         <p>Launch date: ${rovers.image.photos[0].rover.launch_date}</p>
@@ -77,7 +91,8 @@ const roverInfo = (rovers) => {
         <p>Date of latest pictures: ${rovers.image.photos[0].earth_date}</p>
         ${latestImages()}
         
-    `)}
+    `)
+    }
 }
 
 // the DropDown function allows us to create a custom dropdown menu
@@ -85,25 +100,29 @@ const DropDown = (option1, option2, option3, option4, buttonId, prompt) => {
     return `
         <select id='${buttonId}'>
         <option disabled selected value> -- ${prompt} -- </option>
+        <option>${option4}</option>
         <option>${option1}</option>
         <option>${option2}</option>
         <option>${option3}</option>
-        <option>${option4}</option>
         </select>
     `
 }
 
 // the roverDropDown function is a high order function that returns a custom dropdown using the DropDown function
 const roverDropDown = (callback) => {
-    return callback("curiosity", "opportunity", "spirit", "perseverance", "roverDropDown", "choose a rover")
+    return callback('curiosity', 'opportunity', 'spirit', 'perseverance', 'roverDropDown', 'choose a rover')
 }
 
 //api call 
 const getRoverApi = (state, rover) => {
-    let { rovers } = state
+    let {
+        rovers
+    } = state
     fetch(`http://localhost:3000/${rover}`)
         .then(res => res.json())
-        .then(rovers => updateStore(store, { rovers }))
+        .then(rovers => updateStore(store, {
+            rovers
+        }))
 
     return this.data
 }
